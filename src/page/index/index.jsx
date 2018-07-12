12ccc50e7c1694f102daf54aa2545e0ca18992aa
fulei <fulei@taoshouyou.com>
2018-07-12 18:27:13 +0800

    配置接口域名

--- src/page/index/index.jsx
+++ src/page/index/index.jsx
@@ -1,7 +1,13 @@
 import React,{Component} from "react";
 
 export default class Index extends Component {
+    constructor() {
+        super(this);
+    }
 
+    getData() {
+        
+    }
 
     render() {
         return (
---
+++ src/utils/api.js
@@ -0,0 +1,16 @@
+let config = {
+    host:'wx.tsy.com',
+    api:{
+        index:'/indexpage/index/index',
+        game:'/games/list/index',
+        goods:{
+            data:'/trades/list/index',
+            list:'/trades/list/indexpager',
+            area:'/trades/list/getarealist',
+            detail:'/trades/list/info'
+        }
+    }
+}
+
+
+export default config ;
