9fa12140a0565eaa89c8265eb11664eeedf11a5f
fulei <648988741@qq.com>
2018-07-31 22:42:14 +0800

    添加react-router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
+import { Switch , Route } from 'react-router-dom'; 
 import './App.css';
 import Index from './page/index/index'
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <Index className="index"></Index>
-      </div>
-    );
+    return (<Switch>
+      <Route exact path='/' component={Index}></Route>
+    </Switch>)
   }
 }
 
